88809994b72dbe80b9116123fd1f0764582f02b0
Ben <benwalls2004@gmail.com>
2026-01-15 18:50:27 -0500

    connection to database from ml service validated

--- backend/recommendation-service/app/helpers.py
+++ backend/recommendation-service/app/helpers.py
@@ -1,8 +1,17 @@
 import sqlite3
 from fastapi import Depends, FastAPI
+import os
 
-DB_PATH = "/app/db/flexlog"
+DB_PATH = os.getenv("DATABASE_PATH", "/data/test_database.db")
 
+def test_db():
+  with sqlite3.connect(DB_PATH) as conn: 
+      conn.row_factory = sqlite3.Row
+      cursor = conn.cursor() 
+      cursor.execute("SELECT * FROM USERS")
+      rows = cursor.fetchall()
+      return [dict(row) for row in rows]
+  
 def get_exercise_features(exercise_name : str):
   with sqlite3.connect(DB_PATH) as conn: 
     cursor = conn.cursor()
--- backend/recommendation-service/app/main.py
+++ backend/recommendation-service/app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
+from app.helpers import test_db
 
 app = FastAPI() 
 
 @app.get("/health")
 def root(): 
   return {"status": "ok"}
+
+@app.get("/db_health")
+def root(): 
+  users = test_db()
+  return users
 
 class RecommendationParams(BaseModel):
   exercise_name: str
