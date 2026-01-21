4a5e6c09513dbbae9628369749f50cd9d0b3db65
Ben <benwalls2004@gmail.com>
2026-01-20 21:48:38 -0500

    model pipeline, saved some test models to configure inference endpoints. Will replace models/ with object storage

--- backend/recommendation-service/app/helpers.py
+++ backend/recommendation-service/app/helpers.py
@@ -1,8 +1,11 @@
 import sqlite3
 from fastapi import Depends, FastAPI
+from pathlib import Path
 import os
 
-DB_PATH = os.getenv("DATABASE_PATH", "/data/test_database.db")
+LOCAL_DB_PATH = Path(__file__).resolve().parents[3] / "data" / "test_database.db"
+DB_PATH = os.getenv("DATABASE_PATH", LOCAL_DB_PATH)
+print(f"using path {DB_PATH}")
 
 def test_db():
   with sqlite3.connect(DB_PATH) as conn: 
@@ -11,9 +14,28 @@ def test_db():
       cursor.execute("SELECT * FROM USERS")
       rows = cursor.fetchall()
       return [dict(row) for row in rows]
-  
-def get_exercise_features(exercise_name : str):
-  with sqlite3.connect(DB_PATH) as conn: 
+
+def get_input_features(user_id: int):
+  with sqlite3.connect(DB_PATH) as conn:
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM EXERCISES WHERE title = {exercise_name}")
-    results = cursor.fetchall()
+    cursor.execute("""
+        SELECT 
+            l.timestamp, l.first,
+            w.name as workout_name,
+            e.variant,
+            e.machine_type,
+            e.name as exercise_name,
+            e.chest, e.back, e.legs, e.shoulders, e.biceps, e.triceps, e.misc_group,
+            e.barbell, e.dumbbell, e.machine, e.cable, e.smith, e.misc_machine,
+            e.isolation, e.compound
+        FROM logs l
+        JOIN exercises e ON l.exercise_id = e.id
+        JOIN workouts w ON l.workout_id = w.id
+        WHERE l.user_id = ?
+        GROUP BY l.exercise_id, l.workout_id
+        ORDER BY l.timestamp ASC
+    """, (user_id,))
+    rows = cursor.fetchall()
+    return rows
+
+ 
--- backend/recommendation-service/app/pipeline.py
+++ backend/recommendation-service/app/pipeline.py
@@ -1,2 +1,63 @@
-DB_path = ""
 
+import argparse
+import pandas as pd
+import joblib
+from pathlib import Path
+from sklearn.linear_model import Ridge
+from app.helpers import get_input_features   
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("--user_id", type=int, required=True, help="user id to run the pipeline for")
+  args = parser.parse_args()
+
+  rows = get_input_features(args.user_id)
+
+  column_names = [
+      "timestamp", "first", "workout_name", 
+      "variant", "machine_type", "exercise_name",
+      "chest","back","legs","shoulders","biceps","triceps","misc_group",
+      "barbell","dumbbell","machine","cable","smith","misc_machine",
+      "isolation","compound"
+  ]
+
+  df = pd.DataFrame(rows, columns=column_names)
+
+  df["exercise_name"] = df["variant"] + " " +  df["machine_type"] + " " + df["exercise_name"]
+
+  muscle_groups = ["chest", "back", "legs", "shoulders", "biceps", "triceps"]
+
+  for group in muscle_groups: 
+    df[f"{group}_day"] = df["workout_name"].str.contains(group, case=False).astype(int) 
+
+  lagged_cols = ["barbell", "dumbbell", "machine", "cable", "smith", "misc_machine", "misc_group", "isolation", "compound", "chest", "back", "legs", "shoulders", "biceps", "triceps"]
+
+  for col in lagged_cols:
+      df[f"prev_{col}"] = (
+          df[col]
+          .shift(1)
+          .fillna(0)
+          .astype(int)
+  )
+
+  df.loc[df["first"] == 1, f"prev_{col}"] = 0
+
+  feature_cols = [col for col in df.columns if col.startswith("prev_") or "day" in col]
+
+  X = df[feature_cols].values
+  y_muscle = df[["chest", "back", "legs", "shoulders", "biceps", "triceps", "misc_group"]]
+  y_machine = df[["barbell", "dumbbell", "machine", "cable", "smith", "misc_machine"]]
+  y_type = df[["isolation", "compound"]] 
+
+  ridge_muscle  = Ridge(alpha=1.0).fit(X, y_muscle)
+  ridge_machine = Ridge(alpha=1.0).fit(X, y_machine)
+  ridge_type    = Ridge(alpha=1.0).fit(X, y_type)
+
+  MODEL_DIR = Path(__file__).resolve().parent.parent / "models"
+
+  MODEL_DIR.mkdir(parents=True, exist_ok=True)
+
+  joblib.dump(ridge_muscle,  MODEL_DIR / f"user_{args.user_id}_muscle.joblib")
+  joblib.dump(ridge_machine, MODEL_DIR / f"user_{args.user_id}_machine.joblib")
+  joblib.dump(ridge_type,    MODEL_DIR / f"user_{args.user_id}_type.joblib")
